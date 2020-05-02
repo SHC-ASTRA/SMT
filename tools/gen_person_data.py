0e68873a2f47d91d0d018070424e401f54207551
Tobin <dukeslaven@gmail.com>
2020-05-01 21:36:30 -0500

    Cronjob hotfix

--- tools/gen_person_data.py
+++ tools/gen_person_data.py
@@ -4,5 +4,5 @@ import requests
 
 # Send request to generate a new datapoint for personnel
 # Should be set as the following cron 0 0 * * 3 - “At 00:00 on Wednesday.”
-r = requests.get('http://localhost:5000/gen_person_data')
+r = requests.get('http://localhost/gen_person_data')
 print(r.content)
